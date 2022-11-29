5ae4c0d6ca93e71ec85404750bbcfcc43157bb1f
Robert Elizondo <rge15@txstate.edu>
2022-11-29 01:59:54 -0600

    Task 1 solution is sufficient but needs work. can move on to next tasks

---
+++ directories.m
@@ -0,0 +1,7 @@
+data_directory = 'C:\Users\Bobby King\Desktop\Computer Vision\Git\Final-Project\Results';
+
+code_directory = 'C:\Users\Bobby King\Desktop\Computer Vision\Git\Final-Project';
+
+training_directory = 'C:\Users\Bobby King\Desktop\Computer Vision\Git\Final-Project\training_test_data';
+
+save directories;
